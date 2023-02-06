e40e3c9ad338924b86c4c109d1003de3f188add2
zx <102405740+KimRRRich@users.noreply.github.com>
2023-02-06 22:49:58 +0800

    主界面以及场景跳转

--- Assets/Scripts/SuperHotSceneManager.cs
+++ Assets/Scripts/SuperHotSceneManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneManager : MonoBehaviour {
+public class SuperHotSceneManager : MonoBehaviour {
 
     public float sceneSpeed;
     private PlayerController player;
--- Assets/Scripts/quitGame.cs
+++ Assets/Scripts/quitGame.cs
@@ -24,6 +24,6 @@ public class quitGame : MonoBehaviour
 
     public void StartGame()
     {
-        //SceneManager.
+        SceneManager.LoadScene("Scene");
     }
 }
